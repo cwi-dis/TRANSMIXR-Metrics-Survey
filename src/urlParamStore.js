45fba8b747ccd84b2f5c16f10ce5a12d77663255
Irene Viola <ireneviola@irenes-mbp.home>
2026-01-22 23:10:34 +0100

    ?

--- src/urlParamStore.js
+++ src/urlParamStore.js
@@ -1,18 +1,31 @@
+// import { writable } from 'svelte/store';
+
+// export const urlParams = writable(new URLSearchParams(window.location.search));
+
+
+// let prevUrl = undefined;
+// setInterval(() => {
+//   const currUrl = window.location.href;
+//   if (currUrl != prevUrl) {
+//     // URL changed
+//     prevUrl = currUrl;
+//     urlParams.set(new URLSearchParams(window.location.search));
+//   }
+  
+// }, 60);
+
 import { writable } from 'svelte/store';
 
 export const urlParams = writable(new URLSearchParams(window.location.search));
 
+window.addEventListener('popstate', () => {
+  urlParams.set(new URLSearchParams(window.location.search));
+});
 
-let prevUrl = undefined;
-setInterval(() => {
-  const currUrl = window.location.href;
-  if (currUrl != prevUrl) {
-    // URL changed
-    prevUrl = currUrl;
-    urlParams.set(new URLSearchParams(window.location.search));
-  }
-  
-}, 60);
+window.addEventListener('hashchange', () => {
+  urlParams.set(new URLSearchParams(window.location.search));
+});
+
 
 // const observeUrlChange = () => {
 //   let oldHref = document.location.href;
